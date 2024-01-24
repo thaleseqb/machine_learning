015bd1824a49c5a0c31c4b51ee2575f05604225c
thaleseqb <thalesquadros2@gmail.com>
2024-01-24 17:22:10 -0300

    adicionar mais funcionalidades à classe

--- gen_data_analyser.py
+++ gen_data_analyser.py
@@ -5,17 +5,24 @@ import numpy as _np
 
 class MLDataAnalysis:
 
-    def __init__(self, uri):
+    def __init__(self, uri, version):
         self.uri = uri
-        self.map = {'yes':1,'sim':1,'não':0,'no':0}
-        self.dic = {
-            ord('a'):_np.array([ord('à'),ord('á'),ord('ã'),ord('â')]),
-            ord('o'): _np.array([ord('ó')]),
-            ord('i'): _np.array([ord('í')]),
-            ord('e'): _np.array([ord('é')]),
-            ord('u'): _np.array([ord('ú')])
-            }
 
+        if 'portuguese' in version:
+            self.map = {'sim':1,'não':0}
+        elif 'english' in version:
+            self.map =  {'yes':1, 'no':0}
+
+        self.dic = {
+                ord('a'):_np.array([ord('à'),ord('á'),ord('ã'),ord('â')]),
+                ord('o'): _np.array([ord('ó')]),
+                ord('i'): _np.array([ord('í')]),
+                ord('e'): _np.array([ord('é')]),
+                ord('u'): _np.array([ord('ú')])
+            }
+        
+        self.data = None
+        
     # por enquanto essa classe é apenas um esqueleto do que eu quero que ela seja futuramente.
     # quero automatizar as manipulações que geralmente são feitas nas tabelas proveniente das
     # bases de dados que podem vir de uri's ou de servidores sql.
@@ -24,17 +31,36 @@ class MLDataAnalysis:
 
     # é interessante identificar quando uma base de dados possui 'sim/yes' e 'não/no'
     # para isso seria interessante fazer uma estrutura de repetição para percorrer os dados e juntamente com o mapa 
-    def reading_uri(self):
+    
+    # @property
+    # def uri(self):
+    #     """."""
+    #     return self.uri
+    
+    # @uri.setter
+    # def uri(self, new_uri):
+    #     """Defines a new uri"""
+    #     self.uri = new_uri
+
+    def _data_set(self):
+        """Defines the data to analyse the dataset"""
+
         if '.csv' in self.uri:
-            data = _pd.read_csv()
+            data = _pd.read_csv(self.uri)
+            self.data = data
+        
+        return data 
 
-    # talvez seja legal agrupar as informações por classificações categóricas e numéricas
-    # acredito que isso não seja exatamente uma tarefa muito simples porque parece ser 
-    # subjetivo, mas é possível tentar.
-
-    # Essa função vai realizar alguns tratamentos necessários na nossa base de dados
     def processing_data(self):
-        pass
+        data = self._data_set()
+        # column_list = data.columns
+
+        # for index, iten in enumerate(column_list):
+        #     column = data[iten]
+        #     if 'sim' in _np.array(column):
+        #         data[iten] = data[iten].map(map)
+
+        return data
 
     # método para retirar as letras que possuem til
     def replace_tilda(strin, dic):
