7ff52af94c2f6d5a593cda69940e3a2f9be514dc
thaleseqb <thalesquadros2@gmail.com>
2024-01-24 14:42:09 -0300

    adicionar função que retira til e outros acentos

--- gen_data_analyser.py
+++ gen_data_analyser.py
@@ -1,13 +1,20 @@
 from sklearn.svm import LinearSVC, SVC
 from sklearn.tree import DecisionTreeClassifier
-import pandas as pd
-import numpy as np
+import pandas as _pd
+import numpy as _np
 
 class MLDataAnalysis:
 
     def __init__(self, uri):
         self.uri = uri
         self.map = {'yes':1,'sim':1,'não':0,'no':0}
+        self.dic = {
+            ord('a'):_np.array([ord('à'),ord('á'),ord('ã'),ord('â')]),
+            ord('o'): _np.array([ord('ó')]),
+            ord('i'): _np.array([ord('í')]),
+            ord('e'): _np.array([ord('é')]),
+            ord('u'): _np.array([ord('ú')])
+            }
 
     # por enquanto essa classe é apenas um esqueleto do que eu quero que ela seja futuramente.
     # quero automatizar as manipulações que geralmente são feitas nas tabelas proveniente das
@@ -19,7 +26,7 @@ class MLDataAnalysis:
     # para isso seria interessante fazer uma estrutura de repetição para percorrer os dados e juntamente com o mapa 
     def reading_uri(self):
         if '.csv' in self.uri:
-            data = pd.read_csv()
+            data = _pd.read_csv()
 
     # talvez seja legal agrupar as informações por classificações categóricas e numéricas
     # acredito que isso não seja exatamente uma tarefa muito simples porque parece ser 
@@ -29,3 +36,20 @@ class MLDataAnalysis:
     def processing_data(self):
         pass
 
+    # método para retirar as letras que possuem til
+    def replace_tilda(strin, dic):
+
+        strin = strin.lower()
+        alfanumeric_string = [ord(alpha_num) for alpha_num in strin]
+        alfanumeric_string = _np.array(alfanumeric_string)
+
+        for iten in dic:
+            bool_array = _np.isin(alfanumeric_string,dic[iten])
+            if True in bool_array:
+                replace_coord = _np.where(bool_array==True)[0]
+                alfanumeric_string[replace_coord] = iten
+
+        transformed_chr = [chr(number) for number in alfanumeric_string]
+        string = ''.join(transformed_chr)
+
+        return string
